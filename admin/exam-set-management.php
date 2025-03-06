eca798b5037f36b83e2471173d37ab61882a8e86
maxsk0211 <media@kbtc.ac.th>
2025-03-06 14:39:35 +0000

    Auto-commit: changes at 2025-03-06 14:39:35

--- admin/exam-set-management.php
+++ admin/exam-set-management.php
@@ -95,7 +95,7 @@ try {
     <link rel="stylesheet" href="../assets/vendor/libs/sweetalert2/sweetalert2.css" />
     <!-- <link rel="stylesheet" href="../assets/vendor/libs/animate/animate.min.css" /> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js" integrity="sha512-hUhvpC5f8cgc04OZb55j0KNGh4eh7dLxd/dPSJ5VyzqDWxsayYbojWyl5Tkcgrmb/RVKCRJI1jNlRbVP4WWC4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Page CSS -->
     <style>
       body {
