d6cbd622a8b00d8418f1f951ecdde30984306cfd
maxsk0211 <media@kbtc.ac.th>
2025-03-06 15:05:35 +0000

    Auto-commit: changes at 2025-03-06 15:05:35

--- admin/exam-set-management.php
+++ admin/exam-set-management.php
@@ -705,7 +705,6 @@ try {
     <script src="../assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
     <!-- <script src="../assets/vendor/libs/moment/moment.min.js"></script>-->
     <script src="../assets/vendor/libs/moment/moment-with-locales.min.js"></script> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js" integrity="sha512-hUhvpC5f8cgc04OZb55j0KNGh4eh7dLxd/dPSJ5VyzqDWxsayYbojWyl5Tkcgrmb/RVKCRJI1jNlRbVP4WWC4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     
     
     <!-- Main JS -->
