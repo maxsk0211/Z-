b5eae0eddcd119ed86c2a4ff035766a4b8b43f18
maxsk0211 <media@kbtc.ac.th>
2025-03-06 14:34:26 +0000

    Auto-commit: changes at 2025-03-06 14:34:26

--- admin/exam-set-management.php
+++ admin/exam-set-management.php
@@ -94,6 +94,7 @@ try {
     <link rel="stylesheet" href="../assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css" />
     <link rel="stylesheet" href="../assets/vendor/libs/sweetalert2/sweetalert2.css" />
     <link rel="stylesheet" href="../assets/vendor/libs/animate/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
     <!-- Page CSS -->
     <style>
