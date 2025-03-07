67d5088e287a9c24df18aeac99d762313d996f05
maxsk0211 <media@kbtc.ac.th>
2025-03-07 07:23:23 +0000

    Auto-commit: changes at 2025-03-07 07:23:23

--- index.php
+++ index.php
@@ -26,7 +26,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     
     <!-- AOS Animation Library -->
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>        :root {
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>        
+    <style>
+    :root {
             --primary-color: #FFD54F;
             --secondary-color: #9C27B0;
             --accent-color: #FF5252;
